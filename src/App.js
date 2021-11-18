1b944c3a6e2096f136180a266d18b0a50de2c3fc
Scott Elwood <scott.elwood@protonmail.com>
2021-11-18 15:52:34 -0500

    modified webpage text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I love my wife
+          My wife is a sweet woman
         </p>
         <a
           className="App-link"
